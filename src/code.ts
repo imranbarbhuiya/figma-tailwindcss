26538aa59716958387a19fdd9635570e654598ae
parbez <imranbarbhuiya.fsd@gmail.com>
2023-10-14 21:02:04 +0530

    fix: remove console logs

--- src/code.ts
+++ src/code.ts
@@ -9,13 +9,9 @@ figma.codegen.on('generate', async (e) => {
 
 	const cssObj = await node.getCSSAsync();
 
-	console.log(cssObj);
-
 	const css = Object.entries(cssObj)
 		.map(([key, value]) => `${key}: ${value.replace(/\/\*.*\*\//g, '').trim()};`)
 		.join('\n');
-
-	console.log(css);
 
 	const { convertedRoot, nodes } = await converter.convertCSS(`
     div {
